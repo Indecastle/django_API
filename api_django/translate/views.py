15c2f68b0b76e2e4fc97404a61e0c538b432699e
markiz <andred9991@gmail.com>
2021-12-28 03:27:07 +0300

    add mymemory | other update views.py

--- api_django/translate/views.py
+++ api_django/translate/views.py
@@ -11,7 +11,7 @@ from rest_framework.views import APIView
 
 from .view_models.translate_result_vm import LanguageSerializer
 
-from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator, MyMemoryTranslator, DeepL
 # Translator = GoogleTranslator(source='auto', target='ru')
 # translated = GoogleTranslator(source='auto', target='de').translate("keep it up, you are awesome")  # output -> Weiter so, du bist großartig
 
@@ -22,23 +22,42 @@ class DeviceViewSchema(AutoSchema):
     Schema customizations for DeviceViewSet
     """
 
+handlers = {
+    "google": lambda source, target, text: GoogleTranslator(source=source, target=target).translate(text),
+    "mymemory": lambda source, target, text: MyMemoryTranslator(source=source, target=target).translate(text),
+}
+
+def translate(translator, source, target, text):
+    result = ''
+
+    try:
+        translated = handlers[translator](source, target, text) if translator in handlers else ''
+        result = translated
+    except Exception as e:
+        print(e)
+
+    return result
+
+
 class TranslateView(APIView):
-    schema = DeviceViewSchema()
+    #schema = DeviceViewSchema()
 
     #param_get_source = openapi.Parameter('source', in_=openapi.IN_QUERY, description='source language', type=openapi.TYPE_STRING, required=True )
+    param_get_translator = openapi.Parameter('translator', openapi.IN_QUERY, 'example: google / mymemory', type=openapi.TYPE_STRING, required=False)
     param_get_source = openapi.Parameter('source', openapi.IN_QUERY, 'source language', type=openapi.TYPE_STRING, required=True)
     param_get_target = openapi.Parameter('target', openapi.IN_QUERY, 'target language', type=openapi.TYPE_STRING, required=True)
     param_get_text = openapi.Parameter('text', openapi.IN_QUERY, 'text to translate', type=openapi.TYPE_STRING, required=True)
     user_response = openapi.Response('response description', LanguageSerializer)
 
-    @swagger_auto_schema(manual_parameters=[param_get_source, param_get_target, param_get_text], responses={200: user_response})
+    @swagger_auto_schema(manual_parameters=[param_get_translator, param_get_source, param_get_target, param_get_text], responses={200: user_response})
     @action(detail=True, methods=['get'])
     def get(self, request):
+        translator = request.query_params.get('translator') or 'google'
         source = request.query_params.get('source')
         target = request.query_params.get('target')
         text = request.query_params.get('text')
 
-        translated = GoogleTranslator(source=source, target=target).translate(text)
+        translated = translate(translator, source, target, text)
 
         result = {"text": translated}
         serializer = LanguageSerializer(result, many=False)
@@ -48,9 +67,10 @@ class TranslateView(APIView):
     @swagger_auto_schema(method='post', request_body=openapi.Schema(
         type=openapi.TYPE_OBJECT,
         properties={
-            'source': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
-            'target': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
-            'text': openapi.Schema(type=openapi.TYPE_STRING, description='string'),
+            'translator': openapi.Schema(type=openapi.TYPE_STRING, description='google'),
+            'source': openapi.Schema(type=openapi.TYPE_STRING, description='en'),
+            'target': openapi.Schema(type=openapi.TYPE_STRING, description='ru'),
+            'text': openapi.Schema(type=openapi.TYPE_STRING, description='hi'),
         },
         responses={
             status.HTTP_200_OK: Schema(
@@ -65,11 +85,12 @@ class TranslateView(APIView):
     ))
     @action(detail=True, methods=['post'])
     def post(self, request):
+        translator = request.data.get('translator') or 'google'
         source = request.data.get('source')
         target = request.data.get('target')
         text = request.data.get('text')
 
-        translated = GoogleTranslator(source=source, target=target).translate(text)
+        translated = translate(translator, source, target, text)
 
         result = {"text": translated}
         serializer = LanguageSerializer(result, many=False)
